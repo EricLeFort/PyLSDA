932f2e3f3e1582e8bb0e69ac9fb01ee7272a46d8
Eric Le Fort <ericlefort@live.com>
2020-02-09 19:18:15 -0500

    Revised non_max_suppression to sort by score and not y2

--- lsda-keras/src/nms.py
+++ lsda-keras/src/nms.py
@@ -1,4 +1,4 @@
-def non_max_suppression(boxes, overlap):
+def non_max_suppression(scored_boxes, overlap):
     """
     Greedily select high-scoring detections and skip detections that are significantly covered by
     a previously selected detection.
@@ -8,19 +8,19 @@ def non_max_suppression(boxes, overlap):
     up in the case of a large number of boxes
 
     Args:
-        boxes (list of Box): 
+        boxes (list of (Box, score)): The scored boxes
         max_overlap (number): The maximum allowed overlap
     Returns:
         -
     """
-    if not boxes:
+    if not scored_boxes:
         return []
 
-    areas = [(box[2]-box[0]+1) * (box[3]-box[1]+1) for box in boxes]
+    areas = [(box[2]-box[0]+1) * (box[3]-box[1]+1) for box, _ in scored_boxes]
 
-    # Sort by y2, record 
-    decorated_boxes = [(box, i) for i, box in boxes]
-    decorated_boxes.sort(key=lambda x: x[0][3])
+    # Sort by score, record original index
+    decorated_boxes = [(box, score, i) for i, (box, score) in enumerate(scored_boxes)]
+    decorated_boxes.sort(key=lambda x: x[1])
 
     pick = [0]*len(boxes)
     counter = 1
@@ -29,20 +29,20 @@ def non_max_suppression(boxes, overlap):
         pick[counter] = i
         counter += 1
 
-        x1 = max(boxes[i][0], max(box[0] for box, _ in decorated_boxes))
-        y1 = max(boxes[i][1], max(box[1] for box, _ in decorated_boxes))
-        x2 = max(boxes[i][2], max(box[2] for box, _ in decorated_boxes))
-        y2 = max(boxes[i][3], max(box[3] for box, _ in decorated_boxes))
+        x1 = max(scored_boxes[i][0][0], max(box[0] for box, _, _ in decorated_boxes))
+        y1 = max(scored_boxes[i][0][1], max(box[1] for box, _, _ in decorated_boxes))
+        x2 = max(scored_boxes[i][0][2], max(box[2] for box, _, _ in decorated_boxes))
+        y2 = max(scored_boxes[i][0][3], max(box[3] for box, _, _ in decorated_boxes))
         width, height = max(0, x2-x1+1), max(0, y2-y1+1)
 
         # Compute the amount of overlap between this box and the others
         intersect = width * height
         overlaps = [intersect / (areas[i] + area - intersect)
-                    for area in areas[[idx for _, idx in decorated_boxes]]]
+                    for area in areas[[idx for _, _, idx in decorated_boxes]]]
 
         # Filter out any boxes which overlap too much
         decorated_boxes = [(box, i)
-                           for (box, i), overlap in zip(decorated_boxes, overlaps)
+                           for (box, _, i), overlap in zip(decorated_boxes, overlaps)
                            if overlap <= max_overlap]
     
     return pick[:counter]
